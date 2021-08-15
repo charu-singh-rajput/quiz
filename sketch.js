b5bc8c211143b5ff03c7cf9c79b596d49b432c67
Charu Singh <55341559+charu-singh-rajput@users.noreply.github.com>
2021-08-15 09:28:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  //Create quiz
+}
+
+
+function draw(){
+  background("pink");
+  
+}
